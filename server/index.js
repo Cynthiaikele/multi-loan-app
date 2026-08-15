5e6cacb6fb9f46476dc08e1967150f73e89f50dc
cynthiaikele <ikelecynthia95@gmail.com>
2026-08-15 14:43:57 +0100

    correcting cors issue

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(helmet());
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(compression());
 app.use(morgan('dev'));
 app.use(express.json());
